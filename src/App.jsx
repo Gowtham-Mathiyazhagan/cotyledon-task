95b2b87adf71f7e9e1addd5944c5fc55b0dd1366
Gowtham-Mathiyazhagan <gowthammathi07@gmail.com>
2024-06-25 23:27:16 +0530

    chor : chages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React,{useState} from "react";
 import Navbar from "./Components/Navbar.jsx";
+import Coupon from "./Components/Coupon.jsx";
 const App = () => {
   const [inputBg, setInputBg] = useState(false);
   return (
     <main onClick={() => setInputBg(false)}>
       <Navbar input={inputBg} setInput={setInputBg}/>
+      <Coupon/>
     </main>
   );
 };
