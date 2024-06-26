835bc77c10b2ec0e294ed1d1f8d19be6fc3152eb
Gowtham-Mathiyazhagan <gowthammathi07@gmail.com>
2024-06-26 14:01:40 +0530

    fix : added changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React,{useState} from "react";
 import Navbar from "./Components/Navbar.jsx";
 import Coupon from "./Components/Coupon.jsx";
+import Carousel from "./Components/Carousel.jsx";
 const App = () => {
   const [inputBg, setInputBg] = useState(false);
   return (
     <main onClick={() => setInputBg(false)}>
       <Navbar input={inputBg} setInput={setInputBg}/>
       <Coupon/>
+      <Carousel/>
     </main>
   );
 };
