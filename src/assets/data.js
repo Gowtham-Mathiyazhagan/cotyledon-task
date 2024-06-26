f04a0d4bec1be2a939d49057452c5268b8d0d5e6
Gowtham-Mathiyazhagan <gowthammathi07@gmail.com>
2024-06-26 14:00:25 +0530

    feat : carousel data implemented

---
+++ src/assets/data.js
@@ -0,0 +1,214 @@
+import b1 from "./images/b1.jpg"
+import b2 from "./images/b2.jpg"
+import b3 from "./images/b3.jpg"
+import b4 from "./images/b4.jpg"
+import b5 from "./images/b5.jpg"
+import b6 from "./images/b6.jpg"
+import b7 from "./images/b7.jpg"
+import b8 from "./images/b8.jpg"
+import b9 from "./images/b9.jpg"
+import b10 from "./images/b10.jpg"
+import b11 from "./images/b11.jpg"
+import b12 from "./images/b12.jpg"
+import b13 from "./images/b13.jpg"
+import b14 from "./images/b14.jpg"
+import b15 from "./images/b15.jpg"
+
+import c1 from "./images/c1.jpg"
+import c2 from "./images/c2.jpg"
+import c3 from "./images/c3.jpg"
+import c4 from "./images/c4.jpg"
+import c5 from "./images/c5.jpg"
+import c6 from "./images/c6.jpg"
+import c7 from "./images/c7.jpg"
+import c8 from "./images/c8.jpg"
+import c9 from "./images/c9.jpg"
+import c10 from "./images/c10.jpg"
+import c11 from "./images/c11.jpg"
+import c12 from "./images/c12.jpg"
+import c13 from "./images/c13.jpg"
+import c14 from "./images/c14.jpg"
+import c15 from "./images/c15.jpg"
+
+export const carousel =[
+	{
+		brand : b1,
+		image : c1,
+		title : "Men fashion",
+		discount :"10-50% off",
+	},
+	{
+		brand : b2,
+		image : c2,
+		title : "Men collection",
+		discount :"upto 50% off",
+	},
+	{
+		brand : b3,
+		image :c3 ,
+		title : "blazzer and its collection",
+		discount :"70% off",
+	},
+	{
+		brand : b4,
+		image : c4,
+		title : "men casuals collection",
+		discount :"min.10% off",
+	},
+	{
+		brand : b5,
+		image : c5,
+		title : "shoes for formal",
+		discount :"80-100%",
+	},
+	{
+		brand : b6,
+		image : c6,
+		title : "Kelsey Formal Shoes",
+		discount :"max.5-10%",
+	},
+	{
+		brand : b7,
+		image : c7,
+		title : "Party Wear",
+		discount :"45-50%",
+	},
+	{
+		brand : b8,
+		image : c8,
+		title : "Girls frock",
+		discount :"min.2-6%",
+	},
+	{
+		brand : b9,
+		image :c9 ,
+		title : "Men fashion",
+		discount :"upto 78%",
+	},
+	{
+		brand : b10,
+		image : c10,
+		title : "Men's LifeStyle Blog",
+		discount :"34-67% max",
+	},
+	{
+		brand : b11,
+		image :c11 ,
+		title : "best version",
+		discount :"45% dic",
+	},
+	{
+		brand : b12,
+		image : c12,
+		title : "Dressing Inspiration",
+		discount :"1% min",
+	},
+	{
+		brand : b13,
+		image : c13,
+		title : "Floral Print",
+		discount :"max.100%",
+	},
+	{
+		brand : b15,
+		image :c14 ,
+		title : "Fashion Dress",
+		discount :"min.37%",
+	},
+	{
+		brand : b1,
+		image : c15,
+		title : "new model",
+		discount :"22%-55%",
+	},
+	{
+		brand : b2,
+		image : c1,
+		title : "wastern style",
+		discount :"45% upto",
+	},
+	{
+		brand : b3,
+		image : c2,
+		title : "indian model",
+		discount :"max.34%-29%",
+	},
+	{
+		brand : b4,
+		image : c3,
+		title : "casuals and king",
+		discount :"56%-dic",
+	},
+	{
+		brand : b5,
+		image : c4,
+		title : "fasion design",
+		discount :"1%-10%",
+	},
+	{
+		brand : b6,
+		image : c5,
+		title : "shooes",
+		discount :"max.69%",
+	},
+	{
+		brand : b7,
+		image :c6 ,
+		title : "Collection and new",
+		discount :"min,10%",
+	},
+	{
+		brand : b8,
+		image :c7 ,
+		title : "fav art",
+		discount :"min.45%",
+	},
+	{
+		brand : b9,
+		image : c8,
+		title : "wedding special",
+		discount :"23-56%",
+	},
+	{
+		brand : b14,
+		image : c9,
+		title : "party dress",
+		discount :"78%-99%",
+	},
+	{
+		brand : b5,
+		image : c5,
+		title : "shoes for formal",
+		discount :"80-100%",
+	},
+	{
+		brand : b7,
+		image :c6 ,
+		title : "Collection and new",
+		discount :"min,10%",
+	},
+	{
+		brand : b2,
+		image : c1,
+		title : "wastern style",
+		discount :"45% upto",
+	},
+	{
+		brand : b3,
+		image : c2,
+		title : "indian model",
+		discount :"max.34%-29%",
+	},
+	{
+		brand : b6,
+		image : c6,
+		title : "Kelsey Formal Shoes",
+		discount :"max.5-10%",
+	},
+	{
+		brand : b7,
+		image : c7,
+		title : " Dress Collection",
+		discount :"45-50%",
+	},
+]
